931fc7d197ffe4dc6a3f6509ff3774e08e6522c5
Pierre Neuvial <pierre.neuvial@curie.fr>
2006-09-05 16:07:50 +0000

    modified sort.arrayCGH arguments for compliance with default method
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MANOR@19622 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sort.arrayCGH.R
+++ R/sort.arrayCGH.R
@@ -5,28 +5,28 @@
 ## Contact: manor@curie.fr
 
 
-sort.arrayCGH <- function(arrayCGH, position.var="Position", chromosome.var="Chromosome")
-  {
-    chrBreak.var <- "LimitChr"
-    posOrder.var <- "PosOrder"
-    chrNum.var <- paste(chromosome.var, "num", sep=".")
-    
-    chr <- chrom(arrayCGH$cloneValues[[chromosome.var]])
-    or <- order(chr, arrayCGH$cloneValues[[position.var]])
-    arrayCGH$cloneValues[[chrNum.var]] <- chr
-    arrayCGH$cloneValues <- arrayCGH$cloneValues[or,]
-
-    ## only keep clones with known position
-    arrayCGH$cloneValues <- arrayCGH$cloneValues[which(arrayCGH$cloneValues[[chrNum.var]]>0), ]
-    l <- dim(arrayCGH$cloneValues)[1]
-    ## position order of each clone along the genome
-    arrayCGH$cloneValues[[posOrder.var]] <- 1:l
-
-    ## flag last clone of each chromosome for genomic representation
-    chr <- arrayCGH$cloneValues[[chrNum.var]]
-    w <- which(chr[1:(l-1)]!=chr[2:l])
-    arrayCGH$cloneValues[[chrBreak.var]] <- 0
-    arrayCGH$cloneValues[[chrBreak.var]][w] <- 1
-
-    arrayCGH
-  }
+sort.arrayCGH <- function(x, decreasing = FALSE, position.var="Position", chromosome.var="Chromosome", ...) {
+  arrayCGH <- x
+  chrBreak.var <- "LimitChr"
+  posOrder.var <- "PosOrder"
+  chrNum.var <- paste(chromosome.var, "num", sep=".")
+  
+  chr <- chrom(arrayCGH$cloneValues[[chromosome.var]])
+  or <- order(chr, arrayCGH$cloneValues[[position.var]])
+  arrayCGH$cloneValues[[chrNum.var]] <- chr
+  arrayCGH$cloneValues <- arrayCGH$cloneValues[or,]
+  
+  ## only keep clones with known position
+  arrayCGH$cloneValues <- arrayCGH$cloneValues[which(arrayCGH$cloneValues[[chrNum.var]]>0), ]
+  l <- dim(arrayCGH$cloneValues)[1]
+  ## position order of each clone along the genome
+  arrayCGH$cloneValues[[posOrder.var]] <- 1:l
+  
+  ## flag last clone of each chromosome for genomic representation
+  chr <- arrayCGH$cloneValues[[chrNum.var]]
+  w <- which(chr[1:(l-1)]!=chr[2:l])
+  arrayCGH$cloneValues[[chrBreak.var]] <- 0
+  arrayCGH$cloneValues[[chrBreak.var]][w] <- 1
+  
+  arrayCGH
+}
