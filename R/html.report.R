868cb6e65f975409b278b27eb03d467e07a6d0fd
Pierre Neuvial <pierre.neuvial@curie.fr>
2007-03-16 13:47:05 +0000

    added a MA plot when intensity normalisation is required
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MANOR@23230 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/html.report.R
+++ R/html.report.R
@@ -6,126 +6,138 @@
 
 
 w <- function(x1="", x2="", x3="", append=TRUE, file=fh)
-  write(paste(x1, x2, x3, sep=""), append=append, file=file)
+    write(paste(x1, x2, x3, sep=""), append=append, file=file)
 
 html.report <- function(...)
-  UseMethod("html.report")
+    UseMethod("html.report")
 
-html.report.arrayCGH <- function(array.norm, array.nonorm=NULL, dir.out=".", array.name=NULL, x="PosOrder", y=c("LogRatioNorm", "LogRatio"), chrLim=NULL, ylim=NULL, zlim=NULL, clim=NULL, light=FALSE, file.name="report", width=10, height=5, ...){
+html.report.arrayCGH <- function(array.norm, array.nonorm=NULL, dir.out=".", array.name=NULL, x="PosOrder", y=c("LogRatioNorm", "LogRatio"), chrLim=NULL, ylim=NULL, zlim=NULL, clim=NULL, intensity=NULL, light=FALSE, file.name="report", width=10, height=5, ...){
 ### outuput file names
-  if (is.null(array.name))
-    array.name <- "arrayCGH"
-  if (!file.exists(dir.out))
-    dir.create(dir.out)
-##  img.nonorm <- paste("nonorm_", array.name, ".png", sep="")
-##  img.norm <- paste("norm_", array.name, ".png", sep="")
-  if (!light)
-    fh <- paste(dir.out, "/", file.name, ".html", sep="")
-  else
-    fh <- paste(dir.out, "/", file.name, sep="")
-  an <- gsub(" ", "_", array.name)
-  img.nonorm <- paste(an, "_nonorm.png", sep="")
-  img.norm <- paste(an, "_norm.png", sep="")
+    if (is.null(array.name))
+        array.name <- "arrayCGH"
+    if (!file.exists(dir.out))
+        dir.create(dir.out)
+    ##  img.nonorm <- paste("nonorm_", array.name, ".png", sep="")
+    ##  img.norm <- paste("norm_", array.name, ".png", sep="")
+    if (!light)
+        fh <- paste(dir.out, "/", file.name, ".html", sep="")
+    else
+        fh <- paste(dir.out, "/", file.name, sep="")
+    an <- gsub(" ", "_", array.name)
+    img.maplot <- paste(an, "_maplot.png", sep="")
+    img.nonorm <- paste(an, "_nonorm.png", sep="")
+    img.norm <- paste(an, "_norm.png", sep="")
 
-  if(length(y)==1) 
-    y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
+    if(length(y)==1) 
+        y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
 
 ### spot- and clone-level flag summary
-  clone.var <- array.norm$cloneValues[[y[2]]]
-  after <- sum(!is.na(clone.var)) ## number of clones after normalization
-  before <- length(clone.var)     ## number of clones before normalization
-  clones <- list(before=before, after=after)
+    clone.var <- array.norm$cloneValues[[y[2]]]
+    after <- sum(!is.na(clone.var)) ## number of clones after normalization
+    before <- length(clone.var)     ## number of clones before normalization
+    clones <- list(before=before, after=after)
 
-  if (!light) {
+    if (!light) {
 ###header of html output file
-    w("<HTML>", append=FALSE, file=fh)
-    w("<HEAD> <TITLE>Normalization report</TITLE> </HEAD>", file=fh)
-    w("<BODY bgcolor=\"lightsteelblue\" text=\"black\" link=\"blue\">", file=fh)
-  }
+        w("<html>", append=FALSE, file=fh)
+        w("<head> <title>Normalization report</title> </head>", file=fh)
+        w("<body bgcolor=\"lightsteelblue\" text=\"black\" link=\"blue\">", file=fh)
+    }
 
-  ## beginning of a big table
-  w("<CENTER>", append=(!light), file=fh)
-  ## report title
-  tit <- paste("MANOR: analysis of ", array.name, sep="")
-  w("<h2><font COLOR=\"purple\">", tit, "</font></h2>", file=fh)
-  w("<h5>", format(Sys.time(), "%Y-%m-%d, %X"), "</h5>", file=fh)
-  w("<TABLE BGCOLOR=\"lightsteelblue\">", file=fh)
+    ## beginning of a big table
+    w("<center>", append=(!light), file=fh)
+    ## report title
+    tit <- paste("MANOR: analysis of ", array.name, sep="")
+    w("<h2><font color=\"purple\">", tit, "</font></h2>", file=fh)
+    w("<h5>", format(Sys.time(), "%Y-%m-%d, %X"), "</h5>", file=fh)
+    w("<table bgcolor=\"lightsteelblue\">", file=fh)
+
+### MA-plot (if intensity is not NULL)
+    if (!is.null(intensity)) {
+        bitmap(paste(dir.out, "/", img.maplot, sep=""), width=width, height=height, pointsize=10, bg="white")
+        intensity.plot(array.norm, M.var=intensity[["M.var"]], A.var=intensity[["A.var"]], pred.var=intensity[["pred.var"]], center.var=intensity[["center.var"]])
+        dev.off()
+        w("<tr align=center><td style=\"font-size:20;color:black;\">M-A plot (span=", intensity[["span"]], ")</font></td></tr>", file=fh)
+        w("<tr><td><img src=\"", img.maplot, "\" alt=\"<M-A plot>\"></td></tr>", file=fh)
+    }
 
 ### genomic- and array-profile without normalization (if array.nonorm is provided)
-  if (!is.null(array.nonorm)) {
-    clone.var.nonorm <- array.nonorm$cloneValues[[y[2]]]
-    if (is.null(ylim))
-      ylim <- c(min(clone.var.nonorm, na.rm=TRUE), max(clone.var.nonorm, na.rm=TRUE))
-    if (is.null(clim))
-      clim <- c(quantile(clone.var.nonorm, 0.05, na.rm=TRUE), quantile(clone.var.nonorm, 0.95, na.rm=TRUE))
-    if (is.null(zlim))
-      zlim <- clim
-    
-    bitmap(paste(dir.out, "/", img.nonorm, sep=""), width=width, height=height, pointsize=10, bg="white")
-##    bitmap(paste(dir.out, "/", img.nonorm, sep=""), pointsize=10, bg="white")
-##    png(filename = paste(dir.out, "/", img.nonorm, sep=""), width = 800, height = 300, pointsize = 10, bg = "white")
-    report.plot(array.nonorm, x=x, y=y, chrLim=chrLim, ylim=ylim, zlim=zlim, clim=clim, ...)
-    dev.off()
-  w("<TR align=center><TD style=\"font-size:20;color:black;\">Profile before normalization: ", clones$before, " clones</font></TD></TR>", file=fh)
-  w("<TR><TD><IMG SRC=\"", img.nonorm, "\" ALT=\"<image without normalization>\"></TD></TR>", file=fh)
-  }
+    if (!is.null(array.nonorm)) {
+        clone.var.nonorm <- array.nonorm$cloneValues[[y[2]]]
+        if (is.null(ylim))
+            ylim <- c(min(clone.var.nonorm, na.rm=TRUE), max(clone.var.nonorm, na.rm=TRUE))
+        if (is.null(clim))
+            clim <- c(quantile(clone.var.nonorm, 0.05, na.rm=TRUE), quantile(clone.var.nonorm, 0.95, na.rm=TRUE))
+        if (is.null(zlim))
+            zlim <- clim
+        
+        bitmap(paste(dir.out, "/", img.nonorm, sep=""), width=width, height=height, pointsize=10, bg="white")
+        ##    bitmap(paste(dir.out, "/", img.nonorm, sep=""), pointsize=10, bg="white")
+        ##    png(filename = paste(dir.out, "/", img.nonorm, sep=""), width = 800, height = 300, pointsize = 10, bg = "white")
+        report.plot(array.nonorm, x=x, y=y, chrLim=chrLim, ylim=ylim, zlim=zlim, clim=clim, ...)
+        dev.off()
+        w("<tr align=center><td style=\"font-size:20;color:black;\">Profile before normalization: ", clones$before, " clones</font></td></tr>", file=fh)
+        w("<tr><td><img src=\"", img.nonorm, "\" alt=\"<image without normalization>\"></td></tr>", file=fh)
+    }
 
 ### genomic- and array-profile with normalization
-  bitmap(paste(dir.out, "/", img.norm, sep=""), width=width, height=height, pointsize=10, bg="white")
-##  bitmap(paste(dir.out, "/", img.norm, sep=""), pointsize=10, bg="white")
-##  png(filename = paste(dir.out, "/", img.norm, sep=""), width = 800, height = 300, pointsize = 10, bg = "white")
-  report.plot(array.norm, x=x, y=y, chrLim=chrLim, ylim=ylim, zlim=zlim, clim=clim, ...)
-  dev.off()
+    bitmap(paste(dir.out, "/", img.norm, sep=""), width=width, height=height, pointsize=10, bg="white")
+    ##  bitmap(paste(dir.out, "/", img.norm, sep=""), pointsize=10, bg="white")
+    ##  png(filename = paste(dir.out, "/", img.norm, sep=""), width = 800, height = 300, pointsize = 10, bg = "white")
+    report.plot(array.norm, x=x, y=y, chrLim=chrLim, ylim=ylim, zlim=zlim, clim=clim, ...)
+    dev.off()
 
-  c2 <- paste(clones$after, " clones (", round(clones$after*100/clones$before), "%) ", sep="")
-  w("<TR align=center><TD style=\"font-size:20;color:black;\">Profile after normalization: ", c2, "</font></TD></TR>", file=fh)
-  w("<TR><TD><IMG SRC=\"", img.norm, "\" ALT=\"<image with normalization>\"></TD></TR>", file=fh)
+    c2 <- paste(clones$after, " clones (", round(clones$after*100/clones$before), "%) ", sep="")
+    w("<tr align=center><td style=\"font-size:20;color:black;\">Profile after normalization: ", c2, "</font></td></tr>", file=fh)
+    w("<tr><td><img src=\"", img.norm, "\" alt=\"<image with normalization>\"></td></tr>", file=fh)
 
 ### table with spot-level flag summary
-  df <- array.norm$flags
-  df$arg[which(is.na(df$arg))] <- "-"
-  w("<TABLE BORDER BGCOLOR=\"lightyellow\">", file=fh)
-  w("<CAPTION align=top><h2><font COLOR=\"black\"> Spot-level flag report </font></h2></CAPTION>", file=fh)
-  if (!is.null(df$label))
-    w("<TR><TH>Flag label</TH> <TH>Flag code</TH> <TH>Parameter</TH> <TH>Flagged spots</TH> <TH>Flagged spots(%)</TH> </TR>", file=fh)
-  else
-    w("<TR><TH>Flag code</TH> <TH>Parameter</TH> <TH>Flagged spots</TH> <TH>Flagged spots(%)</TH> </TR>", file=fh)
-  for (i in 1:length(df$char)) {
-    if (!is.null(df$label))
-      line <- paste(df$label[i], df$char[i], df$arg[i], df$count[i], round(100*df$count[i]/sum(df$count)), sep="</TH> <TH>")
-    else
-      line <- paste(df$char[i], df$arg[i], df$count[i], round(100*df$count[i]/sum(df$count)), sep="</TH> <TH>")
-    w("<TR><TH>", line, "</TH></TR>", file=fh)
-  }
-  w("</TABLE>", file=fh)
-
-### table with array-level quality criteria
-  qdf <- array.norm$quality
-  if (!is.null(qdf)) {
-    w("<TABLE BORDER BGCOLOR=\"lightyellow\">", file=fh)
-    w("<CAPTION align=top> <h2><font COLOR=\"black\"> Quality criteria </font></h2></CAPTION>", file=fh)
-    w("<TR><TH>Label</TH><TH>Value</TH> </TR>", file=fh)
-    for (i in 1:(dim(qdf)[1])) {
-      line <- paste(qdf$label[i], qdf$score[i], sep="</TH> <TH>")
-      w("<TR><TH>", line, "</TH></TR>", file=fh)
+    df <- array.norm$flags
+    if (!is.null(df)) {
+        df$arg[which(is.na(df$arg))] <- "-"
+        w("<table border bgcolor=\"lightyellow\">", file=fh)
+        w("<caption align=top><h2><font color=\"black\"> Spot-level flag report </font></h2></caption>", file=fh)
+        if (!is.null(df$label))
+            w("<tr><th>Flag label</th> <th>Flag code</th> <th>Parameter</th> <th>Flagged spots</th> <th>Flagged spots(%)</th> </tr>", file=fh)
+        else
+            w("<tr><th>Flag code</th> <th>Parameter</th> <th>Flagged spots</th> <th>Flagged spots(%)</th> </tr>", file=fh)
+        for (i in 1:length(df$char)) {
+            if (!is.null(df$label))
+                line <- paste(df$label[i], df$char[i], df$arg[i], df$count[i], round(100*df$count[i]/sum(df$count)), sep="</th> <th>")
+            else
+                line <- paste(df$char[i], df$arg[i], df$count[i], round(100*df$count[i]/sum(df$count)), sep="</th> <th>")
+            w("<tr><th>", line, "</th></tr>", file=fh)
+        }
+        w("</table>", file=fh)
     }
-    w("</TABLE>", file=fh)
-  }
+    
+### table with array-level quality criteria
+    qdf <- array.norm$quality
+    if (!is.null(qdf)) {
+        w("<table border bgcolor=\"lightyellow\">", file=fh)
+        w("<caption align=top> <h2><font color=\"black\"> Quality criteria </font></h2></caption>", file=fh)
+        w("<tr><th>Label</th><th>Value</th> </tr>", file=fh)
+        for (i in 1:(dim(qdf)[1])) {
+            line <- paste(qdf$label[i], qdf$score[i], sep="</th> <th>")
+            w("<tr><th>", line, "</th></tr>", file=fh)
+        }
+        w("</table>", file=fh)
+    }
 
-  ## end of big table
-  w("</TABLE></CENTER>", file=fh)
-  
-  if (!light) {
-    w("</BODY>", file=fh)
-    w("</HTML>", file=fh)
-  }
+    ## end of big table
+    w("</table></center>", file=fh)
+    
+    if (!light) {
+        w("</body>", file=fh)
+        w("</html>", file=fh)
+    }
 }
 
 html.report.default <- function(spot.data, clone.data=NULL, flag.data=NULL, quality.data=NULL, ...) {
 ### create an object of type arrayCGH
-  array.norm <- list(arrayValues=spot.data, cloneValues=clone.data, quality=quality.data, flags=flag.data)
-  class(array.norm) <- "arrayCGH"
-  
+    array.norm <- list(arrayValues=spot.data, cloneValues=clone.data, quality=quality.data, flags=flag.data)
+    class(array.norm) <- "arrayCGH"
+    
 ### apply html.report.arrayCGH
-  html.report.arrayCGH(array.norm, array.nonorm=NULL, ...)
+    html.report.arrayCGH(array.norm, array.nonorm=NULL, ...)
 }
--- R/plots.R
+++ R/plots.R
@@ -6,106 +6,114 @@
 
 
 genome.plot <- function(...)
-  UseMethod("genome.plot")
+    UseMethod("genome.plot")
 
 genome.plot.arrayCGH <- function(arrayCGH, x="PosOrder", y="LogRatio", chrLim=NULL, col.var=NULL, clim=NULL, cex=NULL, pch=NULL, ...)
-  {
+{
     xx <- arrayCGH$cloneValues[[x]]
     yy <- as.numeric(arrayCGH$cloneValues[[y]])
 
     data <- data.frame(x=xx, y=yy)
 
     if (!is.null(chrLim)) 
-      data <- cbind(data, chrLim=arrayCGH$cloneValues[[chrLim]])
+        data <- cbind(data, chrLim=arrayCGH$cloneValues[[chrLim]])
 
     if (is.null(col.var)) {
-      col.var <- y
+        col.var <- y
     }
     z <- arrayCGH$cloneValues[[col.var]]
     
     if (class(z)=="numeric") {
-      if (length(clim)!=2)
-        clim <- c(quantile(z, 0.05, na.rm=TRUE), quantile(z, 0.95, na.rm=TRUE))
-      max <- clim[2]
-      min <- clim[1]
-      col <- myPalette("green", "red", "yellow")
-      c <- (z-min)/(max-min)*((z>=min))*((z<=max))
-      c[z>=max] <- 1
-      mycol <- col[1+floor((length(col)-1)*c)]
+        if (length(clim)!=2)
+            clim <- c(quantile(z, 0.05, na.rm=TRUE), quantile(z, 0.95, na.rm=TRUE))
+        max <- clim[2]
+        min <- clim[1]
+        col <- myPalette("green", "red", "yellow")
+        c <- (z-min)/(max-min)*((z>=min))*((z<=max))
+        c[z>=max] <- 1
+        mycol <- col[1+floor((length(col)-1)*c)]
     }
 
     else {
-      mycol <- as.factor(z)
-      l <- length(levels(mycol))
-      levels(mycol) <- rainbow(l)
+        mycol <- as.factor(z)
+        l <- length(levels(mycol))
+        levels(mycol) <- rainbow(l)
     }
 
     data <- cbind(data, col=mycol)
     genome.plot.default(data, cex=cex, pch=pch, ...)
-  }
+}
 
 genome.plot.default <- function(data, pch=NULL, cex=NULL, xlab="", ylab="", ...)
-  {
+{
     ## default parameters
     pch <- if(is.null(pch))
-      20
+        20
     else pch
 
     cex <- if(is.null(cex))
-      0.5
+        0.5
     else cex
 
     xlab <- if(!nchar(xlab))
-      "Genome position"
+        "Genome position"
     else
-      xlab
+        xlab
     
     ylab <- if(!nchar(ylab))
-      "DNA Copy Number Variation"
+        "DNA Copy Number Variation"
     else ylab
 
     if(!is.null(data$col))
-      plot(data$y~data$x, col=as.character(data$col), pch=pch, cex=cex, xlab=xlab, ylab=ylab, ...)
+        plot(data$y~data$x, col=as.character(data$col), pch=pch, cex=cex, xlab=xlab, ylab=ylab, ...)
     else
-      plot(data$y~data$x, pch=pch, cex=cex, xlab=xlab, ...)
+        plot(data$y~data$x, pch=pch, cex=cex, xlab=xlab, ...)
     
     if (!is.null(data$chrLim))
-      {
+    {
         w <- which(data$chrLim==1)
         for (j in 1:length(w)) abline(v=data$x[w[j]], lty=2, col="black")
-      }
-  }
+    }
+}
 
 report.plot.arrayCGH <- function(arrayCGH, x="PosOrder", y=c("LogRatioNorm", "LogRatio"), chrLim=NULL, layout=TRUE, main=NULL, zlim=NULL, ...) {
-  if (layout)
-    layout(matrix(c(1,2,3,3), 2,2), height=c(7,1), width=c(1,3))
-##        compute.layout.genome(arrayCGH$arrayDesign[1], arrayCGH$arrayDesign[2], bar=bar, surf=surf)
-  if(length(y)==1)
-    y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
-  y.spot <- y[1]
-  y.clone <- y[2]
+    if (layout)
+        layout(matrix(c(1,2,3,3), 2,2), height=c(7,1), width=c(1,3))
+    ##        compute.layout.genome(arrayCGH$arrayDesign[1], arrayCGH$arrayDesign[2], bar=bar, surf=surf)
+    if(length(y)==1)
+        y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
+    y.spot <- y[1]
+    y.clone <- y[2]
 
-  arrayPlot(arrayCGH, y.spot, main="Array image", bar="h", mediancenter=TRUE, layout=FALSE, zlim=zlim)
-  if (is.null(main))
-    main <- "Pan-genomic representation"
-  genome.plot(arrayCGH, x=x, y=y.clone, main=main, chrLim=chrLim, ...)
+    arrayPlot(arrayCGH, y.spot, main="Array image", bar="h", mediancenter=TRUE, layout=FALSE, zlim=zlim)
+    if (is.null(main))
+        main <- "Pan-genomic representation"
+    genome.plot(arrayCGH, x=x, y=y.clone, main=main, chrLim=chrLim, ...)
 }
 
 report.plot.default <- function(spot.data, clone.data, design, x="PosOrder", y=c("LogRatioNorm", "LogRatio"), chrLim=NULL, layout=TRUE, main=NULL, zlim=NULL, ...) {
-  arrayCGH <- list(arrayValues=spot.data, cloneValues=clone.data, arrayDesign=design)
-  class(arrayCGH) <- "arrayCGH"
-  if (layout)
-    layout(matrix(1:2,1,2), width=c(1,4))
-  if(length(y)==1)
-    y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
-  y.spot <- y[1]
-  y.clone <- y[2]
+    arrayCGH <- list(arrayValues=spot.data, cloneValues=clone.data, arrayDesign=design)
+    class(arrayCGH) <- "arrayCGH"
+    if (layout)
+        layout(matrix(1:2,1,2), width=c(1,4))
+    if(length(y)==1)
+        y <- c(y,y)  ## if only one signal variable name is provided, the same name is taken for clone and spot-level analysis
+    y.spot <- y[1]
+    y.clone <- y[2]
 
-  my.arrayPlot.arrayCGH(arrayCGH, y.spot, main="Array image", bar="horizontal", mediancenter=TRUE, zlim=zlim)
-  if (is.null(main))
-    main <- "Pan-genomic representation"
-  genome.plot(arrayCGH, x=x, y=y.clone, main=main, chrLim=chrLim, ...)
+    my.arrayPlot.arrayCGH(arrayCGH, y.spot, main="Array image", bar="horizontal", mediancenter=TRUE, zlim=zlim)
+    if (is.null(main))
+        main <- "Pan-genomic representation"
+    genome.plot(arrayCGH, x=x, y=y.clone, main=main, chrLim=chrLim, ...)
 }
 
 report.plot <- function(...)
-  UseMethod("report.plot")
+    UseMethod("report.plot")
+
+intensity.plot <- function(arrayCGH, M.var="LogRatio", A.var="LogInt", pred.var="iPred", center.var=NULL) {
+    data <- arrayCGH$arrayValues
+    if (!is.null(center.var))
+        data[[M.var]] <- center(data, M.var, center.var)
+    plot(data[[A.var]], data[[M.var]], xlab="A", ylab="M", pch='.')
+    points(data[[A.var]], data[[pred.var]], col="red", pch=".")
+}
