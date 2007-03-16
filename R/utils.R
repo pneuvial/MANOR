e5ace74dbd4c750c4e783a0d088945de4e355ee4
Pierre Neuvial <pierre.neuvial@curie.fr>
2007-03-16 13:50:51 +0000

    added function getChromosomeArm
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MANOR@23232 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -7,28 +7,28 @@
 
 ### coefficient of variation
 cv <- function(x)
-  sd(2^x, na.rm=TRUE)/mean(2^x, na.rm=TRUE)
+    sd(2^x, na.rm=TRUE)/mean(2^x, na.rm=TRUE)
 
 ### rename variables of a data.frame
 rename <- function(data, old.vars, new.vars)
-  {
+{
     if (length(old.vars)!=length(new.vars))
-      warning("old.vars and new.vars have different lengths...\n")
+        warning("old.vars and new.vars have different lengths...\n")
     m <- match(old.vars, names(data))
     w <- which(is.na(m))
     if (length(w))
-      {
+    {
         for (i in 1:length(w)) warning(paste("Variable not found: ", old.vars[w[i]], "\n", sep=""))
         m <- m[-w]
         new.vars <- new.vars[-w]
-      }
+    }
     names(data)[m] <- new.vars
     data
-  }
+}
 
 ### aggregate.data.frame (light version)
 my.aggregate.data.frame <- function (x, by, FUN, ...)
-  {
+{
     x <- as.data.frame(x)
     b <- x[, by]
     y <- lapply(x, tapply, list(b), FUN, ..., simplify = FALSE)
@@ -37,11 +37,11 @@ my.aggregate.data.frame <- function (x, by, FUN, ...)
     y <- data.frame(lapply(y, unlist, use.names = FALSE))
     names(y) <- c(names(x))
     y
-  }
+}
 
 ## from character/factor to numeric chromosome index...
 chrom <- function(c)
-  {
+{
     char.c <- as.character(c)
     char.c[(char.c=="X")] <- 23
     char.c[(char.c=="Y")] <- 24
@@ -49,43 +49,43 @@ chrom <- function(c)
     char.c[(char.c=="")] <- 0
     chr <- as.numeric(char.c)
     chr
-  }
+}
 
 flag.aggregate.arrayCGH <- function(x)
-  # transfer clone-level information to all spots from a clone
-  {
+                                        # transfer clone-level information to all spots from a clone
+{
     if (length(grep("V", x)))
-      # bad CLONE (the 3 spots have to be excluded)
-      ret <- "V"
+                                        # bad CLONE (the 3 spots have to be excluded)
+        ret <- "V"
     else
-      {
+    {
         if (length(grep("T", x)))
-           # bad CLONE (the 3 spots have to be excluded)
-          ret <- "T"
+                                        # bad CLONE (the 3 spots have to be excluded)
+            ret <- "T"
         else
-          ret <- paste(x, collapse="")
-      }
+            ret <- paste(x, collapse="")
+    }
     ret
-  }
+}
 
 ## check that data.names contain test.names
 check.names <- function(test.names, data.names, optional=TRUE) {
-  w <- which(is.na(match(test.names, data.names)))
-  if (length(w)) {
-    for (i in 1:length(w)) warning(paste("Variable not found: ", test.names[w[i]], "\n", sep=""))
-    if (!optional)
-      stop ("Mandatory variables not found")
-    test.names <- test.names[-w]
-  }
-  test.names
+    w <- which(is.na(match(test.names, data.names)))
+    if (length(w)) {
+        for (i in 1:length(w)) warning(paste("Variable not found: ", test.names[w[i]], "\n", sep=""))
+        if (!optional)
+            stop ("Mandatory variables not found")
+        test.names <- test.names[-w]
+    }
+    test.names
 }
 
 ## add empty lines for design compatibility (pseudo-gpr files)
 add.lines <- function(data, sep="_")
-  {
+{
     df <- NULL
     if (dim(data)[2]!=3)
-      stop("Wrong dimension for input data")
+        stop("Wrong dimension for input data")
     x1 <- data[[1]]
     x2 <- data[[2]]
     x3 <- data[[3]]
@@ -97,23 +97,56 @@ add.lines <- function(data, sep="_")
     patron <- paste(test$x1, test$x2, test$x3, sep=sep)
 
     if(dim(test)[1]!=dim(r)[1])
-      {
+    {
         ## design is now compatible with the number of rows in the input file
         w <- which(is.na(match(patron, sample)))              # locate discrepencies
         s <- matrix(unlist(strsplit(patron[w], sep)), ncol=3, byrow=TRUE)
         df <- as.data.frame(s)
         names(df) <- names(data)
-      }
+    }
     df
-  }
+}
 
 center <-  function(data, var, by.var) {
-  index.name <- "zzz.row.index"
-  data[[index.name]] <- 1:(dim(data)[1])
-  a <- aggregate(data[[var]], list(b=data[[by.var]]), median, na.rm=TRUE)
-  var.median <- paste(var, ".median", sep="")
-  names(a)[2] <- var.median
-  m <- merge(data, a, by.y="b", by.x=by.var, all.x=TRUE, sort=FALSE)
-  m <- m[order(m[[index.name]]),]
-  m[[var]]-m[[var.median]]
+    index.name <- "zzz.row.index"
+    data[[index.name]] <- 1:(dim(data)[1])
+    a <- aggregate(data[[var]], list(b=data[[by.var]]), median, na.rm=TRUE)
+    var.median <- paste(var, ".median", sep="")
+    names(a)[2] <- var.median
+    m <- merge(data, a, by.y="b", by.x=by.var, all.x=TRUE, sort=FALSE)
+    m <- m[order(m[[index.name]]),]
+    m[[var]]-m[[var.median]]
+}
+
+getChromosomeArm <- function(arrayCGH, chrVar="Chromosome", posVar="Position") {
+    data(cytoband)
+    chrArmVar <- "ChromosomeArm"
+    chrNumVar <- paste(chrVar, "num", sep=".")
+    centromere <- cytoband[which(cytoband$Centro==1),]
+
+    w <- NULL
+    for (u in unique(centromere$Chromosome)) {
+        wu <- which(centromere$Chromosome==u)
+        w <- c(w, wu[which.max(centromere[["Start"]])])
+    }
+
+    centromere <- centromere[w,c("Chromosome","Start")]
+    centromere$Chromosome <- ChrNumeric(centromere$Chromosome)
+    names(centromere) <- c(chrNumVar,"CentroPos")
+
+    arrayCGH$arrayValues[[chrNumVar]] <- ChrNumeric(arrayCGH$arrayValues[[chrVar]])
+    arrayCGH$arrayValues <- merge(arrayCGH$arrayValues, centromere, all.x=TRUE)
+    arrayCGH$arrayValues[[chrArmVar]] <- "p"
+    indNA <- which(is.na(arrayCGH$arrayValues[[chrVar]]))
+    if (length(indNA))
+        arrayCGH$arrayValues[[chrArmVar]][indNA] <- NA
+
+    ind <- which(arrayCGH$arrayValues[[posVar]]>arrayCGH$arrayValues$CentroPos)
+    arrayCGH$arrayValues[[chrArmVar]][ind] <- "q"
+
+    if (length(indNA)<dim(arrayCGH$arrayValues)[1])
+        arrayCGH$arrayValues[[chrArmVar]][-indNA] <- paste(arrayCGH$arrayValues[[chrVar]][-indNA], arrayCGH$arrayValues[[chrArmVar]][-indNA], sep="")
+    else
+        arrayCGH$arrayValues[[chrArmVar]] <- paste(arrayCGH$arrayValues[[chrVar]], arrayCGH$arrayValues[[chrArmVar]], sep="")
+    arrayCGH
 }
